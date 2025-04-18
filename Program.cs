48aa6564d19116fac0b309d0d4c162f343489b63
yamachu <yamachu.dev@gmail.com>
2025-04-18 23:33:58 +0900

    Env経由にしてコード自体は書き換えなくていいようにする

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NotifyAgentActMCP.Services;
@@ -9,6 +10,7 @@ builder.Logging.AddConsole(consoleLogOptions =>
     // Configure all logs to go to stderr
     consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
 });
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services
     .AddMcpServer()
@@ -16,13 +18,13 @@ builder.Services
     .WithToolsFromAssembly();
 
 builder.Services
-    .AddSingleton(sp =>
+    .AddSingleton(_ =>
     {
-        // REPLACE_HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        var openJTalkDictPath = "/Users/yamachu/Projects/github.com/yamachu/NotifyAgentActMCP/voicevox_core/dict/open_jtalk_dic_utf_8-1.11";
-        var voiceModelPath = "/Users/yamachu/Projects/github.com/yamachu/NotifyAgentActMCP/voicevox_core/models/vvms/0.vvm";
-        var onnxruntimePath = "/Users/yamachu/Projects/github.com/yamachu/NotifyAgentActMCP/bin/Debug/net9.0/libvoicevox_onnxruntime.1.17.3.dylib";
-        uint styleId = 1; // REPLACE_HERE
+        var config = builder.Configuration;
+        var openJTalkDictPath = config["OPEN_JTALK_DICT_PATH"]!;
+        var voiceModelPath = config["VOICE_MODEL_PATH"]!;
+        var onnxruntimePath = config["ONNXRUNTIME_PATH"]!;
+        uint styleId = uint.TryParse(config["VOICEVOX_STYLE_ID"], out var sid) ? sid : 1;
 
         var service = VoicevoxTtsService.Build(openJTalkDictPath, voiceModelPath, styleId, onnxruntimePath).Result;
         return service;
